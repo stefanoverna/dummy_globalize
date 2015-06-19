e454de81e4a0ffcd72559480dddc961e430a4a61
Lorenzo Masini <l.masini@cantierecreativo.net>
2015-06-19 10:30:34 +0200

    .with_locale method

--- lib/dummy_globalize.rb
+++ lib/dummy_globalize.rb
@@ -3,3 +3,20 @@ require "dummy_globalize/version"
 if defined?(ActiveRecord::Base)
   require 'dummy_globalize/translates'
 end
+
+module DummyGlobalize
+  class << self
+    def with_locale(locale, &block)
+      previous_locale = I18n.locale
+
+      begin
+        I18n.locale = locale
+        result = yield(locale)
+      ensure
+        I18n.locale = previous_locale
+      end
+
+      result
+    end
+  end
+end
